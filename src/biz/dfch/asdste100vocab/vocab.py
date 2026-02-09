b48160bf0615d891cfd7e282bc49de3d0a02d30d
Ronald Rink <dfch@users.noreply.github.com>
2026-02-09 13:45:24 +0100

    misc
    * update README
    * update LICENSE
    * add spelling

--- src/biz/dfch/asdste100vocab/vocab.py
+++ src/biz/dfch/asdste100vocab/vocab.py
@@ -1,19 +1,19 @@
-# Copyright (c) 2026 Ronald Rink, http://d-fens.ch
+# Copyright (C) 2026 Ronald Rink, d-fens GmbH, http://d-fens.ch
 #
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# GNU Affero General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
+# You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
-# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-License-Identifier: AGPL-3.0-or-later
 
 """Vocabulary class."""
 
@@ -199,10 +199,7 @@ class Vocab:
         return word.name.lower()
 
     def sort(
-        self,
-        *,
-        key: Callable[[Word], str] | None = None,
-        reverse=False
+        self, *, key: Callable[[Word], str] | None = None, reverse=False
     ) -> None:
         """
         Sort the `Word` items in the vocabulary.
